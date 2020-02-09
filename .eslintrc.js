d377037419298294cd34f12490dcad3548b5f6b0
Cauê dos Santos Silva <cauedossantossilva@hotmail.com>
2020-02-09 09:21:36 -0300

    feat: Acessando API do github

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     'prettier'
   ],
   rules: {
+    'react/state-in-constructor': [true, 'always'],
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': [
       'warn',
